5de363d9386e6f6b00edbbf907d51754c26f8dce
Zhongxiao Li <lzx325@outlook.com>
2019-08-04 19:22:49 +0300

    changed features

--- algorithms/alg_tetrisFeatures.m
+++ algorithms/alg_tetrisFeatures.m
@@ -2,13 +2,13 @@ function [phi]=alg_tetrisFeatures(bd,pc,a,params)
     n_features=4;
     phi=zeros(n_features,1);
     phi(1)=1;
-    bd_height=boardHeight(bd);
+    %bd_height=boardHeight(bd);
 %     phi(2)=max(bd_height);
     %phi(3)=min(bd_height);
     mv=params.moves{pc}{a};
     [bd_new,rw] = nextBoard(bd,mv);
     bd_new_height=boardHeight(bd_new);
-    phi(3)=max(bd_new_height);
+    phi(2)=max(bd_new_height);
     %phi(5)=min(bd_new_height);
 %     phi(4)=rw;
 end
